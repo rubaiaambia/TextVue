b14277021297425ac3496894f46bea757b2d6647
Rubaia Ambia <rubaiaambia1597@gmail.com>
2021-03-14 23:29:39 -0400

    Initial Commit

---
+++ TextVue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextVue
+//
+//  Created by Rubaia Ambia on 3/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
